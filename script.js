136c08324ca6d3cc5108dc1d160b1804b4a068d1
Edmond P <edmondpark1@gmail.com>
2020-01-14 17:39:13 -0800

    Updated JS file with const basics for now

---
+++ script.js
@@ -0,0 +1,10 @@
+class Calculator{
+
+}
+
+const bill = document.getElementById('bill'); //get value of bill by doing bill.value;
+const serviceType = document.getElementById('serviceType');
+const serviceQuality = document.getElementById('serviceQuality');
+const numPeople = document.getElementById('numPeople');
+
+const calculateButton = document.querySelector('calculate');
