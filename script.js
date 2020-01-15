5e3017120fca5e700ecefba7f67f494ef02dde12
Edmond P <edmondpark1@gmail.com>
2020-01-14 18:09:14 -0800

    added calculating function

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
-class Calculator{
+let tipPercentage = 0;
+let tipAmount = 0;
+let tipAmountPerPerson = 0;
 
+const calculateButton = document.querySelector('#calculateButton');
+
+function calculateTip(){
+    bill = document.getElementById('bill').value; 
+    serviceType = document.getElementById('serviceType').value;
+    serviceQuality = document.getElementById('serviceQuality').value;
+    numPeople = document.getElementById('numPeople').value;
+
+    switch(serviceType){
+        case 'Server':
+            tipPercentage=.175;
+            break;
+        case 'Bartender':
+            tipPercentage=.15;
+            break;
+        case 'Barber':
+            tipPercentage=.15;
+            break;
+        case 'Ride Service':
+            tipPercentage=.15;
+            break;
+        case 'Food Delivery':
+            tipPercentage=.2;
+            break;
+        case 'Hotel Room Service':
+            tipPercentage=.15;
+            break;
+    }
+
+    if(serviceQuality === 'Bad'){
+        tipPercentage-=.05;
+    } 
+    else if(serviceQuality === 'Good'){
+        tipPercentage+=.05;
+    }
+
+    tipAmount = bill*tipPercentage;
+
+    tipAmountPerPerson = 0; //resetting value
+    if(numPeople == 0) return;
+    tipAmountPerPerson = tipAmount/numPeople;
 }
 
-const bill = document.getElementById('bill'); //get value of bill by doing bill.value;
-const serviceType = document.getElementById('serviceType');
-const serviceQuality = document.getElementById('serviceQuality');
-const numPeople = document.getElementById('numPeople');
+function updateDisplay(){
+    let netTotal = document.createTextNode("Total with tip: " + tipAmount);
+    let netTotal = document.createTextNode("Total with tip: " + tipAmount);
+}
 
-const calculateButton = document.querySelector('calculate');
+calculateButton.addEventListener('click', button => {
+    calculateTip();
+    updateDisplay();
+});
